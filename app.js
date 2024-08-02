b4344f4dc5964bbb99c6c78edec31276628558f0
Eduardo Hernández Villa <ehernandezvilla@gmail.com>
2024-08-02 13:09:39 -0400

    adding api logic

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
-var http = require('http');
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    var message = 'It works!\n',
-        version = 'NodeJS ' + process.versions.node + '\n',
-        response = [message, version].join('\n');
-    res.end(response);
+// app.js
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// Function to read JSON data from file
+const readJsonFile = (filePath) => {
+  return JSON.parse(fs.readFileSync(path.resolve(__dirname, filePath)));
+};
+
+// Endpoint to get users
+app.get('/users', (req, res) => {
+  const data = readJsonFile('data.json');
+  res.json(data.users);
 });
-server.listen();
+
+// Endpoint to get products
+app.get('/products', (req, res) => {
+  const data = readJsonFile('data.json');
+  res.json(data.products);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
